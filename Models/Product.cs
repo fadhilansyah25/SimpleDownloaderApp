38d5cb62f0f44158b887345c0f067b2684f08b16
fadhilansyah25 <fadhilansyah25@gmail.com>
2025-05-21 20:17:37 +0700

    Init project

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace SimpleDownloaderApp.Models;
+
+public class Product
+{
+    public int ProductID { get; set; }
+    public string Name { get; set; } = "";
+    public string SubCategory { get; set; } = "";
+    public decimal ListPrice { get; set; }
+}
